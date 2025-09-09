10664a832a7ad886b55b784e17b16b3848ad6a41
kbaik <>
2025-09-09 16:32:58 -0700

    update inline markdown converstion to handle TextType.IMAGE and TextType.LINK

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,16 @@
+import re
 from textnode import TextType, TextNode
+
+
+def text_to_textnodes(text):
+    textnodes = [TextNode(text, TextType.TEXT)]
+    textnodes = split_nodes_delimiter(textnodes, "**", TextType.BOLD)
+    textnodes = split_nodes_delimiter(textnodes, "_", TextType.ITALIC)
+    textnodes = split_nodes_delimiter(textnodes, "`", TextType.CODE)
+    textnodes = split_nodes_image(textnodes)
+    textnodes = split_nodes_link(textnodes)
+    return textnodes
+
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -36,3 +48,68 @@ def text_delimiter(text, delimiter, text_type):
     
     return new_nodes
 
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        image_list = extract_markdown_images(node.text)
+        if not image_list:
+            new_nodes.append(node)
+            continue
+
+        current_idx = 0
+        for image in image_list:
+            img_url = f"![{image[0]}]({image[1]})"
+            start_idx = node.text.find(img_url)
+            end_idx = start_idx + len(img_url)
+            if start_idx > current_idx:
+                new_nodes.append(TextNode(node.text[current_idx:start_idx], TextType.TEXT))
+            new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+            current_idx = end_idx
+
+        if current_idx < len(node.text):
+            new_nodes.append(TextNode(node.text[current_idx:], TextType.TEXT))
+        
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        link_list = extract_markdown_links(node.text)
+        if not link_list:
+            new_nodes.append(node)
+            continue
+
+        current_idx = 0
+        for link in link_list:
+            link_url = f"[{link[0]}]({link[1]})"
+            start_idx = node.text.find(link_url)
+            end_idx = start_idx + len(link_url)
+            if start_idx > current_idx:
+                new_nodes.append(TextNode(node.text[current_idx:start_idx], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            current_idx = end_idx
+
+        if current_idx < len(node.text):
+            new_nodes.append(TextNode(node.text[current_idx:], TextType.TEXT))
+
+    return new_nodes
+
+
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
